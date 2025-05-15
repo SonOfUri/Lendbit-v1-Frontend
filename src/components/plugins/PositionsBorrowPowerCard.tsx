52958cadb8e306e6e4ee1259f452253ed08cbae8
Okste <okste1234@gmail.com>
2025-05-15 12:02:42 +0100

    FEAT: clean

--- src/components/plugins/PositionsBorrowPowerCard.tsx
+++ src/components/plugins/PositionsBorrowPowerCard.tsx
@@ -2,32 +2,34 @@ import React from "react";
 import DashboardBorrowPowerCard from "./DashboardBorrowPowerCard.tsx";
 
 type PositionsBorrowPowerCardProps = {
-    percentage: number;
-    totalCollateral: string;
-    availableToBorrow: string;
+	percentage: number;
+	totalCollateral: string;
+	availableToBorrow: string;
 };
 
 const PositionsBorrowPowerCard: React.FC<PositionsBorrowPowerCardProps> = ({
-                                                             percentage,
-                                                             totalCollateral,
-                                                             availableToBorrow,
-                                                         }) => {
-    return (
-        <div className="rounded-md bg-[#050505] rounded-md text-white  w-full noise shadow-1">
-            {/* Top summary section */}
-            <div className="flex justify-between text-sm p-4 rounded-t-md font-light px-2 bg-[#181919]">
-                <p>
-                    Total Collateral: <span className="font-semibold">{totalCollateral}</span>
-                </p>
-                <p>
-                    Available to Borrow: <span className="font-semibold">{availableToBorrow}</span>
-                </p>
-            </div>
+	percentage,
+	totalCollateral,
+	availableToBorrow,
+}) => {
+	return (
+		<div className="rounded-md bg-[#050505] text-white  w-full noise shadow-1">
+			{/* Top summary section */}
+			<div className="flex justify-between text-sm p-4 rounded-t-md font-light px-2 bg-[#181919]">
+				<p>
+					Total Collateral:{" "}
+					<span className="font-semibold">{totalCollateral}</span>
+				</p>
+				<p>
+					Available to Borrow:{" "}
+					<span className="font-semibold">{availableToBorrow}</span>
+				</p>
+			</div>
 
-            {/* Core Borrow Power chart */}
-            <DashboardBorrowPowerCard percentage={percentage} />
-        </div>
-    );
+			{/* Core Borrow Power chart */}
+			<DashboardBorrowPowerCard percentage={percentage} />
+		</div>
+	);
 };
 
 export default PositionsBorrowPowerCard;
